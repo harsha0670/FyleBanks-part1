582684daecc92d398da82fe42fb3fd00fb387735
Harsha N <harsha0670@gmail.com>
2021-02-05 15:44:36 +0530

    allowed heroku app

--- DjangoREST/settings.py
+++ DjangoREST/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://desolate-meadow-48761.herokuapp.com/']
 
 
 
